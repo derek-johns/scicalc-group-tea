410939f2712a9c0c9b3f36df0e8c4bcc632b0de6
derek <derek@zipcodes-MacBook-Pro.local>
2020-10-31 15:25:12 -0400

    Added Trig Functions.

--- calculator.py
+++ calculator.py
@@ -1,3 +1,5 @@
+import math
+
 class Calculator:
 
     def __init__(self):
@@ -7,6 +9,41 @@ class Calculator:
         return x + y
 
     def sub(self, x, y):
-        return 0
+        return x - y
 
+    def exponentiation(self, x, y):
+        return x ** y
+
+    def inverse(self, x):
+        return 1 / x
+
+    def invert_sign(self, x):
+        return -x
+
+    def display_error(self):
+        pass
+
+    def sine(self, x):
+        return math.sin(x)
+
+    def cosine(self, x):
+        return math.cos(x)
+
+    def tangent(self, x):
+        return math.tan(x)
+
+    def inverse_sine(self, x):
+        return math.asin(x)
+
+    def inverse_cosine(self, x):
+        return math.acos(x)
+
+    def inverse_tan(self, x):
+        return math.atan(x)
+
+    def switchUnitsMode(self):
+        pass
+
+    def switchUnitsMode(self, mode):
+        pass
 # add lots more methods to this calculator class.

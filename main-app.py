3d9ad419787ef9e68fd44bb5c7e50dc3144b0bab
amanda <amanda@zipcodes-MacBook-Pro.local>
2020-10-31 13:08:20 -0400

    Menu structure, added memory functions to calc, updated performCalcLoop in main-app to call menu.py

--- main-app.py
+++ main-app.py
@@ -1,5 +1,11 @@
 from calculator import Calculator
+import sys
+import menu
 
+m = [0]
+m1 = [0]
+display = 0
+mode = "decimal"
 
 def getTwoNumbers():
     a = float(input("first number? "))
@@ -9,19 +15,31 @@ def getTwoNumbers():
 
 def displayResult(x: float):
     print(x, "\n")
+    m.append(x)
+    #print(m)
+    #print(m1)
+    return m
 
 
 def performCalcLoop(calc):
+    print("Welcome to the calculator")
     while True:
-        choice = input("Operation? ")
+        menu.run_menu(calc)
+        #print(result)
+        #displayResult(result)
+        #if result == "exit":
+            #pass #user exits the calculator
+        """choice = input("Operation? ")
         if choice == 'q':
             break  # user types q to quit calulator.
         elif choice == 'add':
             a, b = getTwoNumbers()
             displayResult(calc.add(a, b))
         else:
-            print("That is not a valid input.")
+            print("That is not a valid input.")"""
 
+"""def exit():
+    result = "exit"""""
 
 # main start
 def main():

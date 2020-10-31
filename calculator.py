68cb13dcc6c1bda66e5e26184d2202a825a996b2
anusha <anusha@zipcodes-MacBook-Pro-6.local>
2020-10-31 00:02:08 -0400

    Scientific Calculator

--- calculator.py
+++ calculator.py
@@ -1,3 +1,5 @@
+import math
+
 class Calculator:
 
     def __init__(self):
@@ -7,6 +9,40 @@ class Calculator:
         return x + y
 
     def sub(self, x, y):
-        return 0
+        return x - y
+
+    def mul(self, x, y):
+        return x * y
+
+    def div(self, x, y):
+        return x / y
+
+    def sqr(self, x):
+        return x ** 2
+
+    def sqroot(self, x):
+        return math.sqrt(x)
+
+    def log(self, x):
+        return math.log10(x)
+
+    def ilog(self, x):
+        return 10 ** x
+
+    def nlog(self, x):
+        return math.log(x)
+
+    def inlog(self, x):
+        return math.exp(x)
+
+    def floor(self,x):
+        return math.floor(x)
+
+    def ceil(self,x):
+        return math.ceil(x)
+
+
+
 
 # add lots more methods to this calculator class.
+    #def mul(self, ):
